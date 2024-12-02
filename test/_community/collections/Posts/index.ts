a8cd4013b4ec64fb0812e54212dd0d92556f0e73
ChannKan <channahk@shefing.com>
2024-12-02 13:03:33 +0200

    field component in rich-text

--- test/_community/collections/Posts/index.ts
+++ test/_community/collections/Posts/index.ts
@@ -1,5 +1,7 @@
 import type { CollectionConfig } from 'payload'
 
+import { BlocksFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
+
 export const postsSlug = 'posts'
 
 export const PostsCollection: CollectionConfig = {
@@ -11,6 +13,56 @@ export const PostsCollection: CollectionConfig = {
     {
       name: 'title',
       type: 'text',
+    },
+    {
+      name: 'richText',
+      type: 'richText',
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          BlocksFeature({
+            blocks: [
+              {
+                slug: 'testblock1',
+                fields: [
+                  {
+                    name: 'testfield',
+                    type: 'text',
+                  },
+                ],
+              },
+            ],
+            inlineBlocks: [
+              {
+                slug: 'myInlineBlock',
+                fields: [
+                  {
+                    name: 'paragraph',
+                    type: 'relationship',
+                    relationTo: 'posts',
+                  },
+                  {
+                    name: 'textToDisplay',
+                    type: 'text',
+                    admin: {
+                      components: {
+                        Field: {
+                          path: '/collections/Posts/FT#IBClientField',
+                        },
+                      },
+                    },
+                  },
+                ],
+                // admin: {
+                //   components: {
+                //     Label: 'test/_community/IBLabel/LabelComponent',
+                //   },
+                // },
+              },
+            ],
+          }),
+        ],
+      }),
     },
   ],
   versions: {
